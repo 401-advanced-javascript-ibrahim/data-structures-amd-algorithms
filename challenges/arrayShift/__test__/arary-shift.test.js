e3948961524812f0003de3006b45fa750cb83c08
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-01-29 12:08:03 +0200

    fixing the bugs and installed eslint

--- challenges/arrayShift/__test__/arary-shift.test.js
+++ challenges/arrayShift/__test__/arary-shift.test.js
@@ -7,7 +7,7 @@ let arr = [1, 2, 5, 4]
 
 describe('testig the validation of insertShiftArray function', () => {
     it('see if the pushing part of the function works', () => {
-        expect(arratShift.push(arr, 3).length).toBeEqual(5)
+        expect(arratShift.push(arr, 3).length).toEqual(5)
     });
 
     it('testing the insertShiftArray function', () => {
