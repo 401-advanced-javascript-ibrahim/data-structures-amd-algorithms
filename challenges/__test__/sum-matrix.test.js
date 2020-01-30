e9cb983c7d838335f54329dab8639e986e05a62f
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-01-30 23:29:32 +0200

    adding eslint and the whiteboard

--- challenges/__test__/sum-matrix.test.js
+++ challenges/__test__/sum-matrix.test.js
@@ -1,22 +1,26 @@
 'use strict';
 
-const functions = require('../lib/sum-matrix.js')
+const functions = require('../lib/sum-matrix.js').default;
 
 describe('Functions of the two interviews', () => {
+
     describe('Sum Matrix Test', () => {
 
         it('Sould sum all element of an array that is an element to an other', () => {
-            const arr = [[1, 2, 3], [3, 5, 7], [1, 7, 10]]
-            expect(functions.sumMatrix(arr)).toEqual([6, 15, 18])
-        })
-    })
+            const arr = [[1, 2, 3], [3, 5, 7], [1, 7, 10]];
+            expect(functions.sumMatrix(arr)).toEqual([6, 15, 18]);
+        });
+    });
+
     describe('Fibonacci Sequence Test', () => {
+
         it('Should return the exact elemen of the given index based on fibonacci sequence', () => {
-            expect(functions.fibSeq(7)).toEqual(13)
-        })
+            expect(functions.fibSeq(7)).toEqual(13);
+        });
+
         it('Should return the element based on fibonacci sequence same as the previuos test if the input is negative', () => {
-            expect(functions.fibSeq(-14)).toEqual(-377)
-        })
-    })
-})
+            expect(functions.fibSeq(-14)).toEqual(-377);
+        });
+    });
+});
 
--- challenges/lib/sum-matrix.js
+++ challenges/lib/sum-matrix.js
@@ -1,34 +1,32 @@
 'use strict';
 
 function sumMatrix(arr) {
-    let result = []
+    let result = [];
     for (let i = 0; i < arr.length; i++) {
         let newIdx = 0;
         for (let j = 0; j < arr[i].length; j++) {
-            newIdx = newIdx + arr[i][j]
-        }
-        result[i] = newIdx
-    }
+            newIdx = newIdx + arr[i][j];
+        };
+        result[i] = newIdx;
+    };
     return result;
-}
+};
 
 function fibSeq(num) {
-    let arr = [0, 1]
+    let arr = [0, 1];
     for (let i = 0; i < Math.abs(num) - 1; i++) {
-        arr[i + 2] = arr[i] + arr[i + 1]
-    }
+        arr[i + 2] = arr[i] + arr[i + 1];
+    };
     if (num >= 0) {
-        return arr[num]
-    }
+        return arr[num];
+    };
     if (num < 0) {
         if (Math.abs(num) % 2 === 0) {
-            return -arr[Math.abs(num)]
+            return -arr[Math.abs(num)];
         } else {
-            return arr[Math.abs(num)]
-        }
-    }
-}
+            return arr[Math.abs(num)];
+        };
+    };
+};
 
-
-
-module.exports = { sumMatrix, fibSeq };
+export default { sumMatrix, fibSeq }
